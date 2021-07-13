036bbf70c5b15ed75e83a99486a696eec0c40876
Eren Satoğlu <satoglu24@gmail.com>
2021-07-14 01:52:58 +0300

    Add project files.

---
+++ ComputerAPP/Controllers/ComputersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerAPP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ComputersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok("Reading Computers");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Reading Computer #{id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]NoteBook noteBook)
+        {
+
+
+            return Ok(noteBook);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] NoteBook noteBook)
+        {
+            return Ok(noteBook);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleting Computer #{id}");
+        }
+    }
+}
